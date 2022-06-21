5d0c537e7ab815c4b508e4a116d28ce3f4bf722b
unknown <ivanalejandroherrera2000@gmail.com>
2022-06-21 18:48:05 -0300

    models created

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,59 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID, //id alfanumerico
+      primaryKey: true,
+      allowNull: false, //campo requerido
+      defaultValue: DataTypes.UUIDV4, //
+    },
+
     name: {
       type: DataTypes.STRING,
+      allowNull: false, //campo requerido
+      unique: true,
+    },
+
+    weight_max: {
+      type: DataTypes.STRING,
+      allowNull: false, //campo requerido
+    },
+
+    weight_min: {
+      type: DataTypes.STRING,
+      allowNull: false, //campo requerido
+    },
+
+    height_max: {
+      type: DataTypes.STRING,
+      allowNull: false, //campo requerido
+    },
+
+    height_min: {
+      type: DataTypes.STRING,
+      allowNull: false, //campo requerido
+    },
+
+    life_time_min: {
+      type: DataTypes.STRING,
+      allowNull: true, //campo NO requerido
+    },
+
+    life_time_max: {
+      type: DataTypes.STRING,
+      allowNull: true, //campo NO requerido
+    },
+
+    image: {
+      type: DataTypes.TEXT,
+      allowNull: true, //campo NO requerido
+    },
+
+    //para saber si es un dog creado por el usuario
+    userCreated: {
+      type: DataTypes.BOOLEAN,
       allowNull: false,
+      defaultValue: true,
     },
   });
 };

9b6e8a2a54085883256bc5ac76947b830b9e4c74
unknown <ivanalejandroherrera2000@gmail.com>
2022-06-27 16:53:22 -0300

    Final changes in controllers

--- api/src/routes/Controller.js
+++ api/src/routes/Controller.js
@@ -1,5 +1,6 @@
 const { Dog, Temperament } = require('../db');
 const axios = require('axios');
+const { API_KEY } = process.env;
 
 //------------------------------GET---------------------------
 
@@ -7,38 +8,20 @@ const axios = require('axios');
 const getDogsAPI = async () => {
   try {
     const apiUrl = await axios.get(
-      `https://api.thedogapi.com/v1/breeds?api_key=232bd982-32a3-43b7-a1e4-bbcb2bdf72bc`
+      `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`
     );
-    const apiInfo = await apiUrl.data.map((e) => {
+    const apiInfo = await apiUrl.data.map((element) => {
       return {
-        name: e.name,
-
-        id: e.id,
-
-        height_min:
-          e.height.metric.split(' - ')[0] && e.height.metric.split(' - ')[0],
-
-        height_max:
-          e.height.metric.split(' - ')[1] && e.height.metric.split(' - ')[1],
-
-        weight_min:
-          e.weight.metric.split(' - ')[0] !== 'NaN'
-            ? e.weight.metric.split(' - ')[0]
-            : 6,
-
-        weight_max:
-          e.weight.metric.split(' - ')[1] && e.weight.metric.split(' - ')[1],
-
-        life_time_min:
-          e.life_span.split(' - ')[0] && e.life_span.split(' - ')[0],
-
-        life_time_max:
-          e.life_span.split(' - ')[1] &&
-          e.life_span.split(' - ')[1].split(' ')[0],
-
-        temperament: e.temperament ? e.temperament : 'Unknown',
-
-        image: e.image.url,
+        id: element.id,
+        name: element.name,
+        image: element.image.url,
+        temperament: element.temperament,
+        weight_min: parseInt(element.weight.metric.slice(0, 2).trim()),
+        weight_max: parseInt(element.weight.metric.slice(4).trim()),
+        height_min: parseInt(element.height.metric.slice(0, 2).trim()),
+        height_max: parseInt(element.height.metric.slice(4).trim()),
+        life_span: element.life_span,
+        breed_group: element.breed_group,
       };
     });
     return apiInfo;
@@ -50,33 +33,16 @@ const getDogsAPI = async () => {
 //Info de la DataBase
 const getDogsDB = async () => {
   try {
-    const dogs = await Dog.findAll({
-      include: Temperament,
+    return await Dog.findAll({
+      include: {
+        model: Temperament, //incluyo el modelo Temperament
+        attributes: ['name'],
+        through: {
+          //validacion donde se constata que traiga solo los atributos señalados de la tabla Temperament
+          attributes: [],
+        },
+      },
     });
-
-    const info = dogs.map((e) => {
-      let temp = e.temperaments.map((e) => e.name);
-      let aux = temp.join(', ');
-      return {
-        name: e.name,
-        id: e.id,
-        userCreated: e.userCreated,
-        height_max: e.height_max,
-        height_min: e.height_min,
-
-        weight_max: e.weight_max,
-        weight_min: e.weight_min,
-
-        life_time_max: e.life_time_max,
-        life_time_min: e.life_time_min,
-
-        temperament: aux,
-        image: e.image
-          ? e.image
-          : 'https://pm1.narvii.com/6893/724dede9a107e0d420269799b4efe8be26a88df9r1-842-1024v2_00.jpg',
-      };
-    });
-    return info;
   } catch (error) {
     console.log('ERROR IN getDogsDB', error);
   }
@@ -85,12 +51,12 @@ const getDogsDB = async () => {
 //Concateno la info de la DB y la API
 const getAllDogs = async () => {
   try {
-    const apiInfo = await getApiInfo();
-    const dbInfo = await getDBinfo();
+    const apiInfo = await getDogsAPI();
+    const dbInfo = await getDogsDB();
     const allInfo = apiInfo.concat(dbInfo);
     return allInfo;
   } catch (error) {
-    console.log('ERROR IN allInfo', error);
+    console.log('ERROR IN getAllInfo', error);
   }
 };
 

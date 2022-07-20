15d0ca4a1c5edba8a954cebed96e7552ff1466e8
unknown <ivanalejandroherrera2000@gmail.com>
2022-07-20 17:54:04 -0300

    Card styles

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from '../styles/Card.module.css';
 
-function Card({ name, img, temperament, weight_min, weight_max }) {
+function Card({ name, img, weight_min, weight_max }) {
   return (
-    <div>
-      <h2>{name}</h2>
-      <img src={img} alt="img not found" width="350px" height="300px" />
-      <h4>{temperament}</h4>
-      <h5>
-        {weight_min} - {weight_max}kg.
+    <div className={styles.container}>
+      <h2 className={styles.name}>{name}</h2>
+      <img
+        className={styles.img}
+        src={img}
+        alt="img not found"
+        width="350px"
+        height="300px"
+      />
+      <h5 className={styles.weight}>
+        {weight_min}kg - {weight_max}kg
       </h5>
     </div>
   );
